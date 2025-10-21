eb39ca3690b6be14d1aff8bd245795c4893e33ce
Carmen Fernández de Córdoba <carmenfdcpwork@gmail.com>
2025-10-21 13:17:46 +0200

    Angular

---
+++ Angular/data-binding/src/app/datos/datos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datos',
+  imports: [],
+  templateUrl: './datos.html',
+  styleUrl: './datos.css'
+})
+export class Datos {
+
+}
---
+++ Angular/primeraWeb/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Tarjeta } from "./tarjeta/tarjeta";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Tarjeta],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('primeraWeb');
+}
