75a5232355c9a2569e5967f034166efbb1a8c040
Carmen Fernández de Córdoba <carmenfdcpwork@gmail.com>
2025-10-23 14:03:06 +0200

    actualizacion del componente table

---
+++ Angular/UD2_03_TableComponent/src/app/table/table.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+type Sexo = 'male' | 'female';
+
+type Alumno = {
+  nombre: string;
+  apellidos: string;
+  edad: number;
+  curso: number;
+  sexo: Sexo;
+}
+
+@Component({
+  selector: 'app-table',
+  imports: [CommonModule],
+  templateUrl: './table.html',
+  styleUrls: ['./table.css'],
+})
+
+export class Table {
+  alumnos: Alumno[] = [
+    { nombre: 'Carmen', apellidos: 'Fernández', edad: 22, curso: 2, sexo: 'female' },
+    { nombre: 'Jorge', apellidos: 'Chacón', edad: 21, curso: 2, sexo: 'male' },
+    { nombre: 'Ana', apellidos: 'Gómez', edad: 19, curso: 1, sexo: 'female' },
+    { nombre: 'Nickita', apellidos: 'Diaz', edad: 21, curso: 2, sexo: 'female' },
+  ];
+
+
+
+
+  getSexIcon(sexo: Sexo) {
+    return sexo === 'male' ? '🧔🏻' : '👧🏼';
+  }
+
+  getSexClass(sexo: Sexo) {
+    return sexo === 'male' ? 'sex-male' : 'sex-female';
+  }
+}
