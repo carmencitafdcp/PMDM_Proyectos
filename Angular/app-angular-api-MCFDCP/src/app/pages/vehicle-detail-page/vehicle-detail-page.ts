b26fdcead4fbc568dcaebab75edac3e1fc5dd931
Carmen Fernández de Córdoba <carmenfdcpwork@gmail.com>
2025-11-17 13:03:05 +0100

    Detalles

---
+++ Angular/app-angular-api-MCFDCP/src/app/pages/vehicle-detail-page/vehicle-detail-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-detail-page',
+  imports: [],
+  templateUrl: './vehicle-detail-page.html',
+  styleUrl: './vehicle-detail-page.css',
+})
+export class VehicleDetailPage {
+
+}
