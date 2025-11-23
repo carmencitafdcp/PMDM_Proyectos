ade9534db27e980a64b19582cf87ae61abcd2b44
Carmen <carmenfdcpwork@gmail.com>
2025-11-23 22:30:33 +0100

    pequeña mejora en las rutas

--- Angular/apiTmdb/src/app/app.routes.ts
+++ Angular/apiTmdb/src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
 import { TopMovieList } from './pages/top-movie-list/top-movie-list';
+import { PeopleDetail } from './pages/people-detail/people-detail';
+import { SearchPage } from './pages/search-page/search-page';
+import { PeopleList } from './pages/people-list/people-list';
+import { MovieDetail } from './pages/movie-detail/movie-detail';
 
 export const routes: Routes = [
     {path: '', component: TopMovieList, pathMatch: 'full'},
     { path: '', redirectTo: 'movies', pathMatch: 'full' },
 
   { path: 'movies', component: TopMovieList },
-  { path: 'movies/:id', component: MovieDetailPage },
+  { path: 'movies/:id', component: MovieDetail },
 
-  { path: 'people', component: PeopleListPage },
-  { path: 'people/:id', component: PeopleDetailPage },
+  { path: 'people', component: PeopleList },
+  { path: 'people/:id', component: PeopleDetail},
 
   { path: 'search', component: SearchPage },
 ];
