919a7a822d65444ac2a056c2ede0a18a56799159
Carmen Fernández de Córdoba <carmenfdcpwork@gmail.com>
2025-11-24 13:49:39 +0100

    pequeños cambios

--- Angular/pruebaMoviesTmdb/src/app/page/home-page/home-page.ts
+++ Angular/pruebaMoviesTmdb/src/app/page/home-page/home-page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PeoplePopularListService } from '../../service/people-popular-lists-service';
 import { MoviePopularListService } from '../../service/movie-popular-list-service';
-import { Movie, moviePopularListResponse } from '../../models/movie-popular-list-response.interface';
-import { People, peoplePopularListResponse } from '../../models/people-popular-list-response.interface';
+import { Movie } from '../../models/movie-popular-list-response.interface';
+import { People } from '../../models/people-popular-list-response.interface';
 
 @Component({
   selector: 'app-home-page',
@@ -21,6 +21,6 @@ export class HomePage implements OnInit {
     });
     this.moviePopularListService.getPopularMovie().subscribe(resp => {
       this.movieList = resp.results;
-    });
+    });  
   }
 }
