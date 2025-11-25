c4c2e35e6b1b008c1325bafadcb3a945de73bc40
Carmen <carmenfdcpwork@gmail.com>
2025-11-25 14:12:57 +0100

    Delete Angular/tmdb-discover directory

--- Angular/tmdb-discover/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { MovieDiscoverPage } from './pages/movie-discover-page/movie-discover-page';
-
-export const routes: Routes = [
-
-    {path: '', pathMatch: 'full', component: MovieDiscoverPage}
-];
