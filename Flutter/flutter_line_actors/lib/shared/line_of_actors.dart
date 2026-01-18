b15bcbd29461fd8cb6d29a7ea43da26ca532e1ee
Carmen <carmenfdcpwork@gmail.com>
2026-01-18 20:03:39 +0100

    LineOfActors sin terminar

--- Flutter/flutter_line_actors/lib/shared/line_of_actors.dart
+++ Flutter/flutter_line_actors/lib/shared/line_of_actors.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
 class LineOfActors extends StatelessWidget {
-  const LineOfActors({super.key});
+  final String nombre;
+  final String apellidos;
+  final String imageUrl;
+
+  const LineOfActors({
+    super.key,
+    required this.nombre,
+    required this.apellidos,
+    required this.imageUrl,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: 180,
+      margin: EdgeInsets.only(right: 12),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+              child: Image.network(
+                imageUrl,
+                height: 240,
+                width: double.infinity,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) =>
+                    Container(height: 240, color: Colors.grey[300]),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
