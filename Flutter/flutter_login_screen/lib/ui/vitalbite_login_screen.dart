e6fd091d90343275b86542eedbaf4f9c5a3497a3
Carmen Fernández de Córdoba <carmenfdcpwork@gmail.com>
2026-01-22 13:10:54 +0100

    cambios en el login page

--- Flutter/flutter_login_screen/lib/ui/vitalbite_login_screen.dart
+++ Flutter/flutter_login_screen/lib/ui/vitalbite_login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 
 class VitalbiteLoginScreen extends StatelessWidget {
@@ -7,58 +9,40 @@ class VitalbiteLoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [ 
+        children: [
           Container(
             width: double.infinity,
             height: double.infinity,
-            decoration: const BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets\fondoVitalBite.png'), fit: BoxFit.cover, ),
-            ),
-          ),
-          Positioned(
-            top: 140,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: Column(
-                children: [
-                  Text(
-                    'Welcome to',
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                      shadows: [Shadow(color: Colors.black54, offset: Offset(2,2), blurRadius: 4)],
-                    ),
-                  ),
-                  Text(
-                    'VitalBite',
-                    style: TextStyle(
-                      fontSize:   44,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      shadows: [Shadow(color: Colors.black87, offset: Offset(3, 3), blurRadius: 8)],
-                    ),
-                  ),
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage('assets/fondoVitalBite.png'),
+                fit: BoxFit.cover,
+              ),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.transparent,
+                  Colors.black.withOpacity(0.6),
                 ],
               ),
             ),
           ),
-          Center(
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 24),
-              width: double.infinity,
-              constraints: const BoxConstraints(maxWidth: 380),
-              padding: const EdgeInsets.all(32),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(28),
-                color: Colors.white.withOpacity(0.12),
-                border: Border.all(color: Colors.white.withOpacity(18), width: 1),
-                boxShadow: [BoxShadow(
-                  color: Colors.black.withOpacity(0.25)
-                )],
+          SafeArea(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Welcome to',
+                    style: TextStyle(
+                      fontSize: 48,
+                    ),
+                  ),       
+                ],
               ),
-            ),
+            )
           ),
         ],
       ),
